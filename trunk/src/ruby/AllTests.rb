c9c409994cac139ce5248c3e589fce79aee01323
skizz <skizz@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-09-17 20:30:25 +0000

    CommandLineBuilder in ruby!!

--- trunk/src/ruby/AllTests.rb
+++ trunk/src/ruby/AllTests.rb
@@ -1,3 +1,61 @@
 require 'test/unit'
 require 'damagecontrol/dependency/AllTraverserTest'
 require 'damagecontrol/dependency/UpstreamDownstreamTraverserTest'
+
+class CommandLineBuilderTest < Test::Unit::TestCase
+
+	def setup
+		@hub = Hub.new
+		@b = CommandLineBuilder.new(@hub, "echo Hello Aslak!")
+	end
+
+	def test_executes_process_and_sends_build_complete_on_receive_build_request
+		@b.receive_message(BuildRequestEvent.new())
+		assert_equal(BuildCompleteEvent.new("Hello Aslak!\n"), @hub.last_message)
+		assert_equal("Hello Aslak!\n", @hub.last_message.result)
+	end
+	
+	def test_doesnt_do_anything_on_other_events
+		@b.receive_message(nil)
+		assert_equal(nil, @hub.last_message)
+	end
+	
+end
+
+class CommandLineBuilder
+	
+	def initialize(hub, command_line)
+		@hub = hub
+		@command_line = command_line
+	end
+	
+	def receive_message(message)
+		result = ""
+		IO.popen(@command_line) {|f| result = f.gets}
+		@hub.publish_message(BuildCompleteEvent.new(result)) if message.is_a? BuildRequestEvent
+	end
+	
+end
+
+class Hub
+	attr_reader :last_message 
+	
+	def publish_message(message)
+		@last_message=message
+	end
+end
+
+class BuildRequestEvent
+end
+
+class BuildCompleteEvent
+	attr_accessor :result
+	
+	def initialize(result)
+		@result = result
+	end
+	
+	def ==(obj)
+		obj.result == result
+	end
+end
