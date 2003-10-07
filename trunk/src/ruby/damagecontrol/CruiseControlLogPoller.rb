83635155eef2da558a25f115a1fe187bf59924cb
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-10-07 15:19:58 +0000

    poller a bit more stable

--- trunk/src/ruby/damagecontrol/CruiseControlLogPoller.rb
+++ trunk/src/ruby/damagecontrol/CruiseControlLogPoller.rb
@@ -20,18 +20,24 @@ module DamageControl
 		end
 
 		def parse_cc_log(file)
-			log = nil
-			File.open(file) do |io|
-				log = REXML::Document.new(io)
+			projectname = "unknown"
+			label = "unknown"
+			begin
+				log = nil
+				File.open(file) do |io|
+					log = REXML::Document.new(io)
+				end
+				info = log.elements['cruisecontrol/info']
+				projectname = info.elements["property[@name='projectname']/@value"].to_s
+				project = Project.new(projectname)
+				label = info.elements["property[@name='label']/@value"].to_s
+			rescue
 			end
-			info = log.elements['cruisecontrol/info']
-			projectname = info.elements["property[@name='projectname']/@value"].to_s
-			project = Project.new(projectname)
-
+			
 			evt = BuildCompleteEvent.new(project)
 			evt.build = Build.new
-			evt.build.label = info.elements["property[@name='label']/@value"].to_s
-			
+			evt.build.label = label
+
 			evt
 		end
 	end
