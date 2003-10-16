0ff0b8956af2d98f83feb63f15071a136303d432
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-10-16 23:55:27 +0000

    Added bootstrapper

--- trunk/src/samples/codehaus.rb
+++ trunk/src/samples/codehaus.rb
@@ -1,5 +1,5 @@
 server = "irc.codehaus.org"
-channel = "#build"
+channel = "#builds"
 
 $damagecontrol_home = File::expand_path('../..')
 $:<<"#{$damagecontrol_home}/src/ruby"
@@ -8,16 +8,21 @@ require 'damagecontrol/SocketTrigger'
 require 'damagecontrol/BuildExecutor'
 require 'damagecontrol/CruiseControlLogPoller'
 require 'damagecontrol/IRCPublisher'
+require 'damagecontrol/BuildBootstrapper'
 
 include DamageControl
 
 build = Build.new("picocontainer")
 build.basedir = "builds"
 build.build_command_line = "maven jar:install"
+
 hub = Hub.new
+
+# all the comps
 BuildExecutor.new(hub)
+BuildBootstrapper.new(hub, "foo").start
 IRCPublisher.new(hub, server, channel).start
-SocketTrigger.new(hub, build).start
+SocketTrigger.new(hub).start
 
 # sleep until ctrl-c
 sleep
