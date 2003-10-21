90599c215c08c165c5324fb8420d0cec41640fcf
mgm <mgm@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-10-21 19:48:57 +0000

    puts instead of print..duh!

--- trunk/src/ruby/damagecontrol/scm/MockSCM.rb
+++ trunk/src/ruby/damagecontrol/scm/MockSCM.rb
@@ -7,13 +7,13 @@ module DamageControl
     
         def handles_path?(path)
             # TODO: Should be debug
-            print "MockSCM.handles_path(#{path})"
+            puts "MockSCM.handles_path(#{path})"
             true
         end
         
         def checkout(path, directory, &proc)
             # TODO: Should be debug
-            print "MockSCM.checkout called"
+            puts "MockSCM.checkout called"
         end
         
     end
