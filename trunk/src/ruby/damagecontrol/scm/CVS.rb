aa975048f87811f2a8c9fa65fd069619379d0ada
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-10-25 19:27:54 +0000

    Fixed DC-1

--- trunk/src/ruby/damagecontrol/scm/CVS.rb
+++ trunk/src/ruby/damagecontrol/scm/CVS.rb
@@ -18,26 +18,50 @@ module DamageControl
       parse_path(path)
     end
     
+    def checkout(path, directory, &proc)
+      directory = File.expand_path(directory)
+      
+      puts "Checking out to " + directory
+      
+      cvsroot, modoole = parse_path(path)[1,2]
+      @filesystem.makedirs(directory)
+      @filesystem.chdir(directory)
+
+      if(checked_out?(directory, modoole))
+        cvs(update_command(cvsroot), &proc)
+      else
+        cvs(checkout_command(cvsroot, modoole, directory), &proc)
+      end
+    end
+
+  private
+
+    def checked_out?(directory, modoole)
+      rootcvs = File.expand_path("#{directory}/#{modoole}/CVS/Root")
+      File.exists?(rootcvs)
+    end
+  
+    def checkout_command(cvsroot, modoole, directory)
+      "-d #{cvsroot} co #{modoole}"
+    end
+
+    def update_command(cvsroot)
+      "-d #{cvsroot} update -d -P"
+    end
+
+    def parse_path(path)
+      /^(:.*:.*):(.*)$/.match(path)
+    end
+    
     def cvs(cmd)
       cmd = "cvs #{cmd}"
-      IO.popen(cmd) do |io|
+      puts cmd
+      io = IO.popen(cmd) do |io|
         io.each_line do |progress|
           yield progress
         end
       end
     end
     
-    def parse_path(path)
-      /^(:.*:.*):(.*)$/.match(path)
-    end
-    
-    def checkout(path, directory, &proc)
-      directory.gsub!('/', '\\') # TODO won't work on linux
-      cvsroot, mod = parse_path(path)[1,2]
-      cvsroot.gsub!('/', '\\') # TODO won't work on linux
-      @filesystem.makedirs(directory)
-      @filesystem.chdir(directory)
-      cvs("-d #{cvsroot} co -d #{File.basename(directory)} #{mod}", &proc)
-    end
   end   
 end
