ec1a3eb991b1f2bca7e3479ce77346509eacd2bc
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-10-27 22:36:55 +0000

    Rewrite. OO this time, so it can be used easily from other Ruby scripts

--- trunk/src/ruby/damagecontrol/ant/ant.rb
+++ trunk/src/ruby/damagecontrol/ant/ant.rb
@@ -1,49 +1,74 @@
-if( ENV['ANT_HOME'] )
-  ANT_HOME = ENV['ANT_HOME']
-else
-  puts "\n\nANT_HOME *MUST* be set!\n\n"  
-  exit(1)
+module Ant
+  # Creates a command line for executing Ant. Can be used
+  # as a standalone script or as a class if called from
+  # a different ruby script.
+  #
+  # If called from another ruby script, the command can be
+  # executed with system or IO.popen
+  #
+  # Author Aslak Hellesoy
+  #
+  # @param javacmd java vm executable
+  # @classpath classpath for ant. if nil, it will be calculated
+  # @ant_home ant installation directory
+  # @ant_opts options to pass to the vm
+  # @args options to pass to ant
+  #
+  def commandline(
+    args      = "", \
+    ant_opts  = ENV['ANT_OPTS'], \
+    ant_home  = ENV['ANT_HOME'],\
+    jikespath = ENV['JIKESPATH'], \
+    java_home = ENV['JAVA_HOME'], \
+    javacmd   = ENV['JAVACMD'], \
+    classpath = ENV['CLASSPATH'] \
+    )
+
+    if( !ant_home )
+      raise "ant_home *MUST* be set!"
+    end
+    
+    if( !javacmd )
+      javacmd = "java"      
+    end
+    
+    if( !classpath )
+      classpath = ""
+      libdir = File.join(ant_home, "lib")
+      Dir.chdir(libdir)
+      jarfiles = Dir["*.{jar,zip}"]
+      jarfiles.each { |jarfile|
+        classpath << "#{File.join(libdir,jarfile)}#{File::PATH_SEPARATOR}"
+      }
+    end
+    if( java_home )
+      tools = File.join(java_home, "lib", "tools.jar")
+      classes = File.join(java_home, "lib", "classes.zip")
+      classpath << "#{File::PATH_SEPARATOR}#{tools}#{File::PATH_SEPARATOR}#{classes}"
+    end
+    
+    if( jikespath )
+      ant_opts << " -Djikes.class.path=#{jikespath}"
+    end
+
+    "#{javacmd} -classpath #{classpath} -Dant.home=#{ant_home} #{ant_opts} org.apache.tools.ant.Main #{args}"
+  end
 end
 
-if( ENV['JAVACMD'] )
-  JAVACMD = ENV['JAVACMD']
-else
-  JAVACMD = "java"
+if( !ENV['ANT_HOME'] )
+  raise "ANT_HOME *MUST* be set!"
 end
 
-if( ENV['CLASSPATH'] )
-  CLASSPATH = ENV['CLASSPATH']
-else
-  CLASSPATH = ""
-  LIBDIR = File.join(ANT_HOME, "lib")
-  Dir.chdir(LIBDIR)
-  jarfiles = Dir["*.{jar,zip}"]
-  jarfiles.each { |jarfile|
-    CLASSPATH << "#{File.join(LIBDIR,jarfile)}#{File::PATH_SEPARATOR}"
-  }
-end
-
-if( ENV['JAVA_HOME'] )
-  JAVA_HOME = ENV['JAVA_HOME']
-  TOOLS = File.join(JAVA_HOME, "lib", "tools.jar")
-  CLASSES = File.join(JAVA_HOME, "lib", "classes.zip")
-  CLASSPATH << "#{CLASSPATH}#{TOOLS}"
-else
+if( !ENV['JAVA_HOME'] )
   puts "\n\nWarning: JAVA_HOME environment variable is not set.\n", \
     "If the build fails because sun.* classes could not be found\n", \
     "you will need to set the JAVA_HOME environment variable\n", \
     "to the installation directory of java\n"
 end
 
-ANT_OPTS = []
-if( ENV['ANT_OPTS'] )
-  ANT_OPTS << ENV['ANT_OPTS'].split
+if($0 == __FILE__)
+  include Ant
+  args = ARGV ? ARGV.join : ""
+  cmdline = commandline(args)
+  system(cmdline)
 end
-if( ENV['JIKESPATH'] )
-  ANT_OPTS << "-Djikes.class.path= ENV['JIKESPATH']"
-end
-
-cmdline = "#{JAVACMD} -classpath #{CLASSPATH} -Dant.home=#{ANT_HOME} #{ANT_OPTS.join} org.apache.tools.ant.Main #{ARGV.join}"
-
-system(cmdline)
-
