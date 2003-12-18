b9d33d37e4225784ac4f84f91783a1eb67070524
dcontrol <dcontrol@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-12-18 11:33:11 +0000

    fixed codehaus startup scripts

--- trunk/src/samples/simple.rb
+++ trunk/src/samples/simple.rb
@@ -8,7 +8,7 @@ require 'damagecontrol/SocketTrigger'
 
 include DamageControl 
 
-def start_simple_server(buildsdir, port = 4711, allow_ips = [127.0.0.1])
+def start_simple_server(buildsdir, port = 4711, allow_ips = ["127.0.0.1"])
   @hub = Hub.new
   @socket_trigger = SocketTrigger.new(@hub, port, allow_ips).start
   scheduler = BuildScheduler.new(@hub)
