b1fd9b3f15945bab29703d636dca95e9b2c2e62c
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-12-18 11:03:35 +0000

    Fixed Codehaus startup script

--- trunk/src/samples/codehaus.rb
+++ trunk/src/samples/codehaus.rb
@@ -13,18 +13,18 @@ require 'damagecontrol/publisher/IRCPublisher'
 require 'damagecontrol/publisher/FilePublisher' 
 require 'damagecontrol/publisher/EmailPublisher' 
 
-include DamageControl 
+include DamageControl
 
 buildRoot = File.expand_path("/var/build") 
  
-hub = Hub.new 
-BuildExecutor.new(hub, "#{buildRoot}/checkout").start
-LogWriter.new(hub, "#{buildRoot}/log")
-FilePublisher.new(hub, "#{buildRoot}/report", HTMLTemplate.new).start 
-IRCPublisher.new(hub, "irc.codehaus.org", "\#damagecontrol", ShortTextTemplate.new).start
-EmailPublisher.new(hub, ShortTextTemplate.new, HTMLTemplate.new, "dcontrol@builds.codehaus.org").start
-SelfUpgrader.new(hub).start
-st = SocketTrigger.new(hub, 4711, ["127.0.0.1", "66.216.68.111"]).start 
+start_simple_server("#{buildRoot}/checkout")
+
+LogWriter.new(@hub, "#{buildRoot}/log")
+FilePublisher.new(@hub, "#{buildRoot}/report", HTMLTemplate.new).start 
+IRCPublisher.new(@hub, "irc.codehaus.org", "\#damagecontrol", ShortTextTemplate.new).start
+EmailPublisher.new(@hub, ShortTextTemplate.new, HTMLTemplate.new, "dcontrol@builds.codehaus.org").start
+SelfUpgrader.new(@hub).start
+st = SocketTrigger.new(@hub, 4711, ["127.0.0.1", "66.216.68.111"]).start 
 
 # wait until ctrl-c 
 st.join 
--- trunk/src/samples/simple.rb
+++ trunk/src/samples/simple.rb
@@ -8,9 +8,9 @@ require 'damagecontrol/SocketTrigger'
 
 include DamageControl 
 
-def start_simple_server(buildsdir)
+def start_simple_server(buildsdir, port = 4711, allow_ips = [127.0.0.1])
   @hub = Hub.new
-  SocketTrigger.new(@hub).start
+  @socket_trigger = SocketTrigger.new(@hub, port, allow_ips).start
   scheduler = BuildScheduler.new(@hub)
   scheduler.add_executor(BuildExecutor.new(@hub, buildsdir))
   scheduler.start
