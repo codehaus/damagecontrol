e118104e5a5457bda93b21847500c248231e394b
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-12-18 11:03:35 +0000

    Fixed Codehaus startup script

--- trunk/src/samples/codehaus.rb
+++ trunk/src/samples/codehaus.rb
@@ -17,14 +17,13 @@ include DamageControl
 
 buildRoot = File.expand_path("/var/build") 
  
-start_simple_server("#{buildRoot}/checkout")
+start_simple_server("#{buildRoot}/checkout", 4711, ["127.0.0.1", "66.216.68.111"])
 
 LogWriter.new(@hub, "#{buildRoot}/log")
 FilePublisher.new(@hub, "#{buildRoot}/report", HTMLTemplate.new).start 
 IRCPublisher.new(@hub, "irc.codehaus.org", "\#damagecontrol", ShortTextTemplate.new).start
 EmailPublisher.new(@hub, ShortTextTemplate.new, HTMLTemplate.new, "dcontrol@builds.codehaus.org").start
 SelfUpgrader.new(@hub).start
-st = SocketTrigger.new(@hub, 4711, ["127.0.0.1", "66.216.68.111"]).start 
 
 # wait until ctrl-c 
-st.join 
+@socket_trigger.join 
