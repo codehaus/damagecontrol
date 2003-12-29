50538c885e131329453b1e3f2edc3d97b860e8a3
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-12-29 17:28:36 +0000

    fixed EmailPublisher

--- trunk/src/ruby/damagecontrol/publisher/EmailPublisher.rb
+++ trunk/src/ruby/damagecontrol/publisher/EmailPublisher.rb
@@ -7,6 +7,7 @@ module DamageControl
   class EmailPublisher < AsyncComponent
 
     attr_reader :always_mail
+
   
     def initialize(channel, subject_template, body_template, from, always_mail=false, server="localhost", port=25)
       super(channel)
