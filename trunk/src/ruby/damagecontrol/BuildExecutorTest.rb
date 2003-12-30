cfe09274daf1ae6f9604363e4effed03cb5d3a1b
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-12-30 16:34:54 +0000

    fixed tests

--- trunk/src/ruby/damagecontrol/BuildExecutorTest.rb
+++ trunk/src/ruby/damagecontrol/BuildExecutorTest.rb
@@ -39,8 +39,8 @@ module DamageControl
       @build = Build.new("damagecontrolled", { "build_command_line" => "bad_command"})
       @build_executor.schedule_build(@build)
       @build_executor.process_next_scheduled_build
-      assert(message_from_hub[-2] instanceof BuildCompleteEvent)
-      assert(message_from_hub[-1] instanceof BuildProgressEvent)
+      assert(messages_from_hub[-2].is_a? BuildProgressEvent)
+      assert(messages_from_hub[-1].is_a? BuildCompleteEvent)
       assert(!messages_from_hub[-1].build.successful)
     end
     
