72b38bdafe4c43006ab646c27c0cf4719f091218
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2003-12-31 01:02:34 +0000

    fail

--- trunk/src/ruby/damagecontrol/BuildSchedulerTest.rb
+++ trunk/src/ruby/damagecontrol/BuildSchedulerTest.rb
@@ -16,6 +16,10 @@ module DamageControl
       @build = Build.new("project")
     end
     
+    def test_fail
+      fail "selftest"
+    end
+    
     def test_build_is_scheduled_on_available_executor
     
       hub.publish_message(BuildRequestEvent.new(@build))
