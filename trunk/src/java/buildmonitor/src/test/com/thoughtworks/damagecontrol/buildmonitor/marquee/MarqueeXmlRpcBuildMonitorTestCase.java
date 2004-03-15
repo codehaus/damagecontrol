b2d86ca0b8bf8948287ed93f633db6acf3166016
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-03-15 19:12:31 +0000

    Changed format

--- trunk/src/java/buildmonitor/src/test/com/thoughtworks/damagecontrol/buildmonitor/marquee/MarqueeXmlRpcBuildMonitorTestCase.java
+++ trunk/src/java/buildmonitor/src/test/com/thoughtworks/damagecontrol/buildmonitor/marquee/MarqueeXmlRpcBuildMonitorTestCase.java
@@ -4,16 +4,18 @@ import com.thoughtworks.damagecontrol.buildmonitor.BuildConstants;
 import com.thoughtworks.damagecontrol.buildmonitor.BuildListener;
 import com.thoughtworks.damagecontrol.buildmonitor.BuildPoller;
 import junit.framework.TestCase;
-import org.jmock.C;
+import org.jmock.Constraint;
 import org.jmock.Mock;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URL;
 import java.net.SocketTimeoutException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,61 +23,32 @@ import java.util.Map;
 
 /**
  * @author Aslak Helles&oslash;y
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class MarqueeXmlRpcBuildMonitorTestCase extends TestCase {
     private IOException serverEx;
 
     public void testResponseIsParsedToListOfMap() throws Exception {
-        final String xmlrpc = "" +
-                "<?xml version=\"1.0\"?>\r\n" +
-                "<methodResponse>\r\n" +
-                "  <params>\r\n" +
-                "    <param>\r\n" +
-                "      <value>\r\n" +
-                "        <array>\r\n" +
-                "          <data>\r\n" +
-                "            <value>\r\n" +
-                "              <struct>\r\n" +
-                "                <member>\r\n" +
-                "                  <name>name</name>\r\n" +
-                "                  <value><string>three</string></value>\r\n" +
-                "                </member>\r\n" +
-                "                <member>\r\n" +
-                "                  <name>status</name>\r\n" +
-                "                  <value><string>failed</string></value>\r\n" +
-                "                </member>\r\n" +
-                "              </struct>\r\n" +
-                "            </value>\r\n" +
-                "            <value>\r\n" +
-                "              <struct>\r\n" +
-                "                <member>\r\n" +
-                "                  <name>name</name>\r\n" +
-                "                  <value><string>blind</string></value>\r\n" +
-                "                </member>\r\n" +
-                "                <member>\r\n" +
-                "                  <name>status</name>\r\n" +
-                "                  <value><string>successful</string></value>\r\n" +
-                "                </member>\r\n" +
-                "              </struct>\r\n" +
-                "            </value>\r\n" +
-                "          </data>\r\n" +
-                "        </array>\r\n" +
-                "      </value>\r\n" +
-                "    </param>\r\n" +
-                "  </params>\r\n" +
-                "</methodResponse>\r\n";
+        String testFile = System.getProperty("expected_xmlrpc_fetch_all_reply");
+        FileReader in = new FileReader (new File(testFile));
+
+        StringBuffer sb = new StringBuffer();
+        int b;
+        while((b = in.read()) != -1) {
+            sb.append((char) b);
+        }
+        String xmlrpc = sb.toString();
 
         final String header = "" +
                 "HTTP/1.1 200 OK\r\n" +
                 "Connection: close\r\n" +
                 "Content-Type: text/xml\r\n" +
-                "Date: Fri, 17 Jul 1998 19:55:08 GMT\r\n" +
-                "Server: UserLand Frontier/5.1.2-WinNT\r\n" +
                 "Content-length: " + xmlrpc.length() + "\r\n" +
                 "\r\n";
 
         final String payload = header + xmlrpc;
+
+        System.out.println(payload);
 
         Thread serverThread = new Thread(new Runnable() {
             public void run() {
@@ -128,20 +101,27 @@ public class MarqueeXmlRpcBuildMonitorTestCase extends TestCase {
         });
         serverThread.start();
 
-        List expectedBuildList = new ArrayList();
+        final List expectedBuildList = new ArrayList();
 
         Map three = new HashMap();
         three.put(BuildConstants.NAME_FIELD, "three");
-        three.put(BuildConstants.STATUS_FIELD, BuildConstants.STATUS_FAILED);
+        three.put(BuildConstants.LAST_BUILD_RESULT_FIELD, BuildConstants.STATUS_FAILED);
+        three.put(BuildConstants.CURRENT_BUILD_STATUS_FIELD, BuildConstants.STATUS_SUCCESSFUL);
         expectedBuildList.add(three);
 
         Map blind = new HashMap();
         blind.put(BuildConstants.NAME_FIELD, "blind");
-        blind.put(BuildConstants.STATUS_FIELD, BuildConstants.STATUS_SUCCESSFUL);
+        blind.put(BuildConstants.LAST_BUILD_RESULT_FIELD, BuildConstants.STATUS_SUCCESSFUL);
+        blind.put(BuildConstants.CURRENT_BUILD_STATUS_FIELD, BuildConstants.STATUS_FAILED);
         expectedBuildList.add(blind);
 
         Mock mockBuildListener = new Mock(BuildListener.class);
-        mockBuildListener.expect("update", C.eq(expectedBuildList));
+        mockBuildListener.expect("update", new Constraint() {
+            public boolean eval(Object o) {
+                assertEquals(expectedBuildList, o);
+                return true;
+            }
+        });
 
         BuildPoller buildMonitor = new MarqueeXmlRpcBuildPoller(new URL("http://localhost:4315/"));
         buildMonitor.addBuildListener((BuildListener) mockBuildListener.proxy());
