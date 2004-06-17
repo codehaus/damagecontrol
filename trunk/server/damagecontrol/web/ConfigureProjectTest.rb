bc8d5fbf825d85332afd8bd0c2b0db2d7aed5aed
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-06-17 18:37:42 +0000

    fixed test

--- trunk/server/damagecontrol/web/ConfigureProjectTest.rb
+++ trunk/server/damagecontrol/web/ConfigureProjectTest.rb
@@ -22,27 +22,27 @@ module DamageControl
     end
 
     def test_provides_content_at_base_url
-      client = Net::HTTP.new("localhost", 4712)
+      client = Net::HTTP.new("localhost", 14712)
       response, data = client.get("/private/admin")
       assert_equal("200", response.code)
     end
 
     def test_creates_new_project_when_complete_project_data_is_posted
-      client = Net::HTTP.new("localhost", 4712)
+      client = Net::HTTP.new("localhost", 14712)
       response, data = client.post("/private/admin", "command=store_configuration&project_name=Chicago")
       assert_equal("200", response.code)
       assert(@server.project_config_repository.project_exists?("Chicago"))
     end
     
     def test_asks_for_project_name
-      client = Net::HTTP.new("localhost", 4712)
+      client = Net::HTTP.new("localhost", 14712)
       response, data = client.get("/private/admin")
       assert_equal("200", response.code)
       assert_match(/Project Name/, data)
     end
 
     def test_fills_in_project_name_if_specified_in_url
-      client = Net::HTTP.new("localhost", 4712)
+      client = Net::HTTP.new("localhost", 14712)
       response, data = client.get("/private/admin?project_name=Milano")
       assert_equal("200", response.code)
       assert_match(/Project Name/, data)
