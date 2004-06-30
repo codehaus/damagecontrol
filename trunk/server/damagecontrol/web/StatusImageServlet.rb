bba9e279382b5fc1aa96c9549b0ba6bc36861edf
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-06-30 07:14:16 +0000

    grey for non-completed builds

--- trunk/server/damagecontrol/web/StatusImageServlet.rb
+++ trunk/server/damagecontrol/web/StatusImageServlet.rb
@@ -34,7 +34,7 @@ module DamageControl
       color = "grey"
       pulse = ""
       build = @build_history_repository.send(last_or_current, project_name)
-      if(!build.nil?)
+      if(!build.nil? && build.completed?)
         color = if build.successful? then "green" else "red" end
         pulse = "-pulse" if @build_scheduler.project_building?(project_name)
       end
