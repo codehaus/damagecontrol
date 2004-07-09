3b26ce03f1bb4af9572f861483822a431c042d2d
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-07-09 01:05:08 +0000

    formatting

--- trunk/server/damagecontrol/scm/AbstractSCM.rb
+++ trunk/server/damagecontrol/scm/AbstractSCM.rb
@@ -6,21 +6,21 @@ module DamageControl
     include FileUtils
     include Logging
     
-    protected
-    
-      attr_reader :config_map
-      attr_reader :working_dir_root
-      
-      def initialize(config_map)
-        @config_map = config_map
-        
-        working_dir_root = config_map["working_dir_root"] || required_config_param("working_dir_root")
-        @working_dir_root = to_os_path(File.expand_path(working_dir_root)) unless working_dir_root.nil?
-      end
-      
-      def required_config_param(param)
-        raise "required configuration parameter: #{param}"
-      end
+  protected
+
+    attr_reader :config_map
+    attr_reader :working_dir_root
+
+    def initialize(config_map)
+      @config_map = config_map
+
+      working_dir_root = config_map["working_dir_root"] || required_config_param("working_dir_root")
+      @working_dir_root = to_os_path(File.expand_path(working_dir_root)) unless working_dir_root.nil?
+    end
+
+    def required_config_param(param)
+      raise "required configuration parameter: #{param}"
+    end
       
   end
 end
