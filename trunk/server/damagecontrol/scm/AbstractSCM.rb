3370a4d51d31534cf0adefc72ec8c23c59fa7187
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-07-14 05:27:44 +0000

    back to trig

--- trunk/server/damagecontrol/scm/AbstractSCM.rb
+++ trunk/server/damagecontrol/scm/AbstractSCM.rb
@@ -38,7 +38,7 @@ module DamageControl
 url = ARGV[0]
 project_name = ARGV[1]
 
-puts "Nudging DamageControl on \#{url} to build project \#{project_name}"
+puts "Trigging build of \#{project_name} DamageControl on \#{url}"
 client = XMLRPC::Client.new2(url)
 build = client.proxy("build")
 result = build.trig(project_name, Time.now.utc.strftime("%Y%m%d%H%M%S"))
