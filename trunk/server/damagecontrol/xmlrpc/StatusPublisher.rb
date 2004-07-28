735f5e5c9f5c198688435b3129882b9b09fcfd76
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-07-28 00:37:53 +0000

    global_search call so JIRA can integrate into DamageControl

--- trunk/server/damagecontrol/xmlrpc/StatusPublisher.rb
+++ trunk/server/damagecontrol/xmlrpc/StatusPublisher.rb
@@ -13,15 +13,42 @@ module XMLRPC
   
     INTERFACE = ::XMLRPC::interface("status") {
       meth 'array history(string)', 'takes project name as string, returns an array of build.', 'history'
-      meth 'array project_names()', 'returns the list of projects registered with the damagecontrol instasnce as an array of strings.', 'project_names'
+      meth 'array project_names()', 'returns the list of projects registered with the damagecontrol instance as an array of strings.', 'project_names'
       meth 'struct current_build(string)', 'takes a project name as string, returns the currently executing build', 'current_build'
       meth 'struct last_completed_build(string)', 'takes a project name as a string, returns the last completed build', 'last_completed_build'
       meth 'struct last_successful_build(string)', 'takes a project name as a string, returns the last successful build', 'last_successful_build'
+      meth 'array global_search(string)', 'returns the build containing text in all projects', 'global_search'
     }
-
+    
     def initialize(xmlrpc_servlet, build_history_repository)
+      @build_history_repository = build_history_repository
       xmlrpc_servlet.add_handler(INTERFACE, build_history_repository)
     end
+    
+    def history(project_name)
+      @build_history_repository.history(project_name)
+    end
+    
+    def current_build(project_name)
+      @build_history_repository.current_build(project_name)
+    end
+    
+    def project_names()
+      @build_history_repository.project_names
+    end
+    
+    def last_completed_build(project_name)
+      @build_history_repository.last_completed_build(project_name)
+    end
+    
+    def last_successful_build(project_name)
+      @build_history_repository.last_successful_build(project_name)
+    end
+    
+    def global_search(text)
+      @build_history_repository.search(text)
+    end
+    
   end
 
 end
