a8fdea0c0212d4c4afd22e90651cfbdc78319c19
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-07-29 23:05:25 +0000

    refactored cmd_with_io into new nice Process class

--- trunk/server/damagecontrol/util/FileUtils.rb
+++ trunk/server/damagecontrol/util/FileUtils.rb
@@ -2,13 +2,85 @@ require 'fileutils'
 require 'ftools'
 require 'damagecontrol/util/Logging'
 
+module Pebbles
+  class Process
+  
+      attr_accessor :command_line
+      attr_accessor :environment
+      attr_accessor :working_dir
+      attr_reader :pid
+      attr_reader :exit_code
+      
+      def initialize
+        @stdout_pipe = IO.pipe
+        @stdin_pipe = IO.pipe
+        
+        @environment = {}
+      end
+    
+      def stdout
+        parent_read
+      end
+      
+      def stdin
+        parent_write
+      end
+    
+      def execute
+        @pid = fork do
+          # in subprocess
+          Dir.chdir(working_dir)
+          environment.each {|key, val| ENV[key] = val}
+          # both processes now have these open
+          # it will not close entirely until both have closed them, which will make the child process hang
+          # so we'll close these now in this process since we are not going to use them
+          parent_read.close
+          parent_write.close
+          $stdin.reopen(child_read)
+          $stdout.reopen(child_write)
+          $stderr.reopen(child_write)
+          exec(command_line)
+        end
+        # both processes now have these open
+        # it will not close entirely until both have closed them, which will make the child process hang
+        # so we'll close these now in this process since we are not going to use them
+        child_read.close
+        child_write.close
+      end
+      
+      def wait
+        ::Process::waitpid(pid)
+        @exit_code = $?
+      end
+  
+      private
+      
+        def parent_read
+          @stdout_pipe[0]
+        end
+        
+        def child_write
+          @stdout_pipe[1]
+        end
+        
+        def parent_write
+          @stdin_pipe[0]
+        end
+        
+        def child_read
+          @stdin_pipe[1]
+        end
+        
+  end
+end
+
 module FileUtils
 
   include DamageControl::Logging
 
   class ProcessFailedException < StandardError
   end
-
+  
   def new_temp_dir(identifier = self)
     identifier = identifier.to_s
     identifier.gsub!(/\(|:|\)/, '_')
@@ -70,37 +142,17 @@ module FileUtils
     end
   end
   
-  def make_relative(basedir, targetdir)
-    targetdir = File.expand_path(targetdir)
-    basedir = File.expand_path(basedir)
-    [File.expand_path(basedir).length+1..-1]
-  end
-  
   def cmd_with_io(dir, cmd, environment = {}, &proc)
     begin
-      parent_rd, child_wr = IO.pipe
-      child_rd, parent_wr = IO.pipe
-      logger.debug("executing #{cmd}")
-      pid = fork do
-        # in subprocess
-        File.mkpath(dir)
-        Dir.chdir(dir)
-        environment.each {|key, val| ENV[key] = val}
-        parent_rd.close
-        parent_wr.close
-        $stdin.reopen(child_rd)
-        $stdout.reopen(child_wr)
-        $stderr.reopen(child_wr)
-        exec(cmd)
-      end
-      # in parent process
-      child_wr.close
-      child_rd.close
-      ret = if proc.arity == 1 then proc.call(parent_rd) else proc.call(parent_rd, parent_wr) end
-      parent_rd.close
-      parent_wr.close
-      Process::waitpid(pid)
-      raise ProcessFailedException.new("'#{cmd}' in directory '#{Dir.pwd}' failed with code #{$?.to_s}") if $? != 0
+      File.mkpath(dir)
+      p = Pebbles::Process.new
+      p.command_line = cmd
+      p.environment = environment
+      p.working_dir = dir
+      p.execute
+      ret = if proc.arity == 1 then proc.call(p.stdout) else proc.call(p.stdout, p.stdin) end
+      p.wait
+      raise ProcessFailedException.new("'#{cmd}' in directory '#{Dir.pwd}' failed with code #{p.exit_code.to_s}") if $? != 0
       logger.debug("successfully executed #{cmd}")
       ret
     rescue NotImplementedError
