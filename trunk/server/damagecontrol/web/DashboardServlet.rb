e13e85b908d9f03be692ed1b8914927cd41df281
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-08-03 06:31:44 +0000

    build scheduler status (queue and executor status) on left hand pane

--- trunk/server/damagecontrol/web/DashboardServlet.rb
+++ trunk/server/damagecontrol/web/DashboardServlet.rb
@@ -27,12 +27,18 @@ module DamageControl
       erb("components/build_executor_status.erb", binding)
     end
     
+    def build_scheduler_status(build_scheduler)
+      build_executors = build_scheduler.executors
+      build_queue = build_scheduler.build_queue.sort {|b1, b2| b1.timestamp_as_time <=> b2.timestamp_as_time }
+      erb("components/build_scheduler_status.erb", binding)
+    end
+    
     def title
       "Dashboard"
     end
     
     def navigation
-      ""
+      build_scheduler_status(build_scheduler)
     end
   
   private
