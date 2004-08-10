b3e2b3f17e27382b3e0effaeeca6c1901fec73f5
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-08-10 02:54:15 +0000

    Allow NoSCM to fake checkout so build process can be run from checkout dir

--- trunk/server/damagecontrol/scm/NoSCM.rb
+++ trunk/server/damagecontrol/scm/NoSCM.rb
@@ -1,9 +1,15 @@
+require 'ftools'
+
 module DamageControl
   require 'damagecontrol/scm/AbstractSCM'
   
   class NoSCM < AbstractSCM
     def method_missing(*args)
       "does nothing :-)"
+    end
+    
+    def checkout(time = nil, &proc)
+      File.mkpath(checkout_dir)
     end
   end
 end
