9730004260f3073783f382446b568493f747706d
tirsen <tirsen@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-08-17 10:41:27 +0000

    DC-190 fixed building installer for windows

--- trunk/server/damagecontrol/Version.rb
+++ trunk/server/damagecontrol/Version.rb
@@ -1,6 +1,8 @@
 module DamageControl
   PRODUCT_NAME = "DamageControl"
-  VERSION = "0.3-prerelease"
+  BUILD = "dev"
+  RELEASE = "0.3-prerelease"
+  VERSION = "#{RELEASE}-#{BUILD}"
   
   VERSION_TEXT = "#{PRODUCT_NAME} version #{VERSION}"
 end
