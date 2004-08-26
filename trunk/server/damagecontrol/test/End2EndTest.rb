8425c41855305b5edda0ff268ec3aac41927a181
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-08-26 06:25:48 +0000

    Trying to kick Subversion

--- trunk/server/damagecontrol/test/End2EndTest.rb
+++ trunk/server/damagecontrol/test/End2EndTest.rb
@@ -237,7 +237,7 @@ class End2EndTest < Test::Unit::TestCase
     #FileUtils.rm_rf(basedir)
   end
   
-  def test_damagecontrol_works_with_cvs
+  def TEMPtest_damagecontrol_works_with_cvs
     cvs = LocalCVS.new(@basedir, "e2e_testproject")
     test_build_and_log_and_irc(cvs)
   end
