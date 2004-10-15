d19a2716e9e2c387ba9e7980d70216108bcb2bb2
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-10-15 06:08:20 +0000

    new method

--- trunk/bin/dctrigger.rb
+++ trunk/bin/dctrigger.rb
@@ -9,7 +9,7 @@ puts "Requesting build of project '#{project_name}' over #{url}"
 client = XMLRPC::Client.new2(url)
 build = client.proxy("build")
 # old syntax
-result = build.trig(project_name, Time.now.utc.strftime("%Y%m%d%H%M%S"))
-#result = build.request(project_name)
+#result = build.trig(project_name, Time.now.utc.strftime("%Y%m%d%H%M%S"))
+result = build.request(project_name)
 puts result
 
