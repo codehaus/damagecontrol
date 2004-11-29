bc3c85ef14eb2791b7460fbac57031772a8c657e
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-11-29 09:55:41 +0000

    add url to notracker and better viewcvs description

--- trunk/server/damagecontrol/scm/NoTracker.rb
+++ trunk/server/damagecontrol/scm/NoTracker.rb
@@ -1,16 +1,18 @@
 require 'damagecontrol/scm/AbstractTracker'
 
 module DamageControl
-	class NoTracker < AbstractTracker
-		public
-		
-			def name
-				"No Tracker"
-			end
-			
-			def ==(other_scm)
+  class NoTracker < AbstractTracker   
+    def name
+      "No Tracker"
+    end
+    
+    def url
+      "#"
+    end
+
+    def ==(other_scm)
       return false if self.class != other_scm.class
       true
     end
-	end
+  end
 end
