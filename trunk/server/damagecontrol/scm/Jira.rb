df30b73a800e1223490bef7315510de4ad2b7aab
aeland <aeland@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-12-04 21:02:01 +0000

    Rewrite regular expression to avoid triggering an infinite loop bug in gsub

--- trunk/server/damagecontrol/scm/Jira.rb
+++ trunk/server/damagecontrol/scm/Jira.rb
@@ -19,9 +19,7 @@ module DamageControl
     def highlight(message)
       url = ensure_trailing_slash(jira_url)
       if(url)
-        message.gsub!(/^([A-Z]+-[0-9]+)/, "<a href=\"#{url}browse/\\1\">\\1</a>")
-        message.gsub!(/(.*[\s])+([A-Z]+-[0-9]+)/, "\\1<a href=\"#{url}browse/\\2\">\\2</a>")
-        message
+	message.gsub(/(?:^|\s)+([A-Z]+-[0-9]+)/, "<a href=\"#{url}browse/\\1\">\\1</a>")
       else
         message
       end
