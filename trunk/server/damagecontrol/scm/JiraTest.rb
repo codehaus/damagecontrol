c7205a1457b050726eaf11aa3eb1f6e6dee9272c
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2004-12-07 08:10:31 +0000

    Improved regexps

--- trunk/server/damagecontrol/scm/JiraTest.rb
+++ trunk/server/damagecontrol/scm/JiraTest.rb
@@ -12,12 +12,12 @@ module DamageControl
       jira = Jira.new
       jira.jira_url = "http://jira.codehaus.org"
       assert_equal("", jira.highlight(''))
-      assert_equal(' <a href="http://jira.codehaus.org/browse/DC-148">DC-148</a>', jira.highlight('DC-148'))
-#      assert_equal('xDC-148', jira.highlight('xDC-148'))
+      assert_equal('<a href="http://jira.codehaus.org/browse/DC-148">DC-148</a>', jira.highlight('DC-148'))
+      assert_equal('x<a href="http://jira.codehaus.org/browse/DC-148">DC-148</a>', jira.highlight('xDC-148'))
+      assert_equal('<a href="http://jira.codehaus.org/browse/DC-148">DC-148</a>:bla', jira.highlight('DC-148:bla'))
       assert_equal('fixed <a href="http://jira.codehaus.org/browse/DC-148">DC-148</a>.', jira.highlight('fixed DC-148.'))
       assert_equal('blahblah<br/>blah<br/>blah <a href="http://jira.codehaus.org/browse/DC-148">DC-148</a>', jira.highlight("blahblah\nblah\nblah DC-148"))
-      assert_equal('blahblah<br/>blah <a href="http://jira.codehaus.org/browse/DC-148">DC-148</a> blah<br/>blah', jira.highlight("blahblah\nblah DC-148 blah\nblah"))
-      assert_equal('blahblah<br/>blah <a href="http://jira.codehaus.org/browse/DC-148">DC-148</a> blah<br/>blah', jira.highlight("blahblah\nblah DC-148 blah\nblah"))
+      assert_equal('ABCABCABCABCABCABCABCABCABCABCABCABC <a href="http://jira.codehaus.org/browse/DC-148">DC-148</a> ABCABCABCABCABCABCABCABCABCABCABCABC', jira.highlight("ABCABCABCABCABCABCABCABCABCABCABCABC DC-148 ABCABCABCABCABCABCABCABCABCABCABCABC"))
     end
   end
 end
