6544eb5fbb37110181af8c53abb92dde616336d6
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-01-01 18:05:11 +0000

    slightly better error msg

--- damagecontrol/trunk/lib/pebbles/XSLT.rb
+++ damagecontrol/trunk/lib/pebbles/XSLT.rb
@@ -8,7 +8,7 @@ module DamageControl
         msg += %{
 This could happen for the following reasons:
 o the xsl or xml was invalid
-o xsltproc is not installed
+o xsltproc is not installed - get it from http://xmlsoft.org/XSLT/downloads.html (libxslt and libxml2)
 o xsltproc might not be on the path
 o xsltproc might be of a version that is incompatible with DamageControl.}
         raise msg
