2e4a9ad26e23c72d645009b9f23a766e07b357b1
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-01-14 17:27:46 +0000

    Fixed DC-353

--- rscm/trunk/app/controllers/application.rb
+++ rscm/trunk/app/controllers/application.rb
@@ -5,11 +5,28 @@ require 'rscm'
 require 'rscm/project'
 require 'rscm/directories'
 
-# Add some generic web capabilities to the RSCM classes
-
 class ApplicationController < ActionController::Base
   include RSCM::Directories
 end
+
+module ActionView
+  module Helpers
+    module UrlHelper
+      # Modify the original behaviour of +link_to+ so that the link
+      # includes the client's timezone as URL param +timezone+ in the request.
+      # Can be used by server to adjust formatting of UTC dates so they match the client's time zone.
+      def convert_confirm_option_to_javascript!(html_options)
+        html_options["onclick"] = "intercept(this);"
+        if html_options.include?(:confirm)
+          html_options["onclick"] += "return confirm('#{html_options[:confirm]}');"
+          html_options.delete(:confirm)
+        end
+      end
+    end
+  end
+end
+
+# Add some generic web capabilities to the RSCM classes
 
 module RSCM
   module Web
@@ -19,7 +36,7 @@ module RSCM
         false
       end
 
-      # Returns the short lowercase name
+      # Returns the short lowercase name. Used for javascript and render_partial.
       def short
         $1.downcase if self.class.name =~ /.*::(.*)/
       end
