89851c64cc50b24d37922803f963bf602f192855
srbaker <srbaker@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-01-26 03:30:51 +0000

    initial commit of Monotone support test

---
+++ rscm/trunk/test/rscm/monotone/monotone_test.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+require 'fileutils'
+require 'rscm/generic_scm_tests'
+require 'rscm/monotone/monotone'
+
+module RSCM
+  class MonotoneTest < Test::Unit::TestCase
+    include GenericSCMTests
+
+    def create_scm(repository_root_dir, path)
+      Monotone.new(repository_root_dir, "MT.db", "com.example.testproject")
+    end
+  end
+end
