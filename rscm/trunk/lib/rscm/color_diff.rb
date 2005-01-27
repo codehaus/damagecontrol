ff87fed2b04f1c3204c6ae5c7cf1c1434f65adf9
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-01-27 00:41:37 +0000

    improved diff parsing - figured it's better to parse into a good model first and then give it a visitor to generate html (or anything else for that matter - like some funky stats). will rename files later.

--- rscm/trunk/lib/rscm/color_diff.rb
+++ rscm/trunk/lib/rscm/color_diff.rb
@@ -1,8 +1,84 @@
-module RSCM
-  class ColorDiff
-  
-    def to_html(diff, out)
-    end
+class String
+  attr_accessor :removed_range
 
+  def removed?
+    (self =~ /\-/) == 0
+  end
+
+  def added?
+    (self =~ /\+/) == 0
+  end
+
+  def content
+    "\n" == self ? "\n" : self[1..-1]
+  end
+
+  def common_prefix_length(o)
+    o = o.content
+    length = 0
+    content.each_byte do |char|
+      break unless o[length] == char
+      length = length + 1
+    end
+    length
+  end
+end
+
+module RSCM
+
+  class Diff
+    attr_reader :minus, :nminus, :plus, :nplus, :removed_range
+
+    def initialize(minus, nminus, plus, nplus)
+      @minus, @nminus, @plus, @nplus = minus, nminus, plus, nplus
+      @lines = []
+    end
+    
+    def <<(line)
+      @lines << line
+    end
+    
+    def line_count
+      @lines.length
+    end
+    
+    def[](n)
+      @lines[n]
+    end
+    
+    def parse
+      prev = nil
+      @lines.each do |line|
+        if(prev && prev.removed? && line.added?)
+          cpl = line.common_prefix_length(prev)
+          puts cpl
+        end
+        prev = line
+      end
+    end
+  end
+
+  class DiffParser
+
+    DIFF_START = /@@ \-([0-9]+),([0-9]+) \+([0-9]+),([0-9]+) @@/
+  
+    def parse_diffs(io)
+      diffs = []
+      diff = nil
+      io.each_line do |line|
+        if(line =~ DIFF_START)
+          diffs << diff if diff
+          diff = Diff.new($1.to_i, $2.to_i, $3.to_i, $4.to_i)
+        elsif(diff)
+          diff << line
+        end
+      end
+      diffs << diff if diff
+      diffs.each do |diff|
+        diff.parse
+      end
+      diffs
+    end
+  
   end
 end
--- rscm/trunk/test/rscm/color_diff_test.rb
+++ rscm/trunk/test/rscm/color_diff_test.rb
@@ -3,20 +3,33 @@ require 'rscm/tempdir'
 require 'rscm/color_diff'
 
 module RSCM
-  class ColorDiffTest < Test::Unit::TestCase
-    def test_should_produce_html_for_diff
-      cd = ColorDiff.new
+  class DiffParserTest < Test::Unit::TestCase
+    def test_should_parse_diff_to_object_model
+      p = DiffParser.new
 
-      html = RSCM.new_temp_dir + "/sample.html"
-      expected_html = File.dirname(__FILE__) + "/simple.html"
-      
       File.open(File.dirname(__FILE__) + "/simple.diff") do |diff|
-        File.open(html, "w") do |out|
-          cd.to_html(diff, out)
-        end
+        diffs = p.parse_diffs(diff)
+        assert_equal(3, diffs.length)
+        assert_equal(7, diffs[1].nplus)
+        
+        assert_equal(7, diffs[0].line_count)
+        assert_equal(8, diffs[1].line_count)
+        assert_equal(9, diffs[2].line_count)
+        
+        assert(!diffs[0][0].removed?)
+        assert(!diffs[0][0].added?)
+        assert(!diffs[0][1].removed?)
+        assert(!diffs[0][1].added?)
+        assert( diffs[0][2].removed?)
+        assert(!diffs[0][2].added?)
+        assert(!diffs[0][3].removed?)
+        assert( diffs[0][3].added?)
+
+        assert_equal("package org.picocontainer.sample.tulip;\n", diffs[0][0].content)
+        assert_equal("\n", diffs[0][1].content)
+        
+        assert_equal(25..34, diffs[0][1].removed_range)
       end
-      
-      assert_equal(File.open(expected_html).read, File.open(html).read)
     end
   end
 end
