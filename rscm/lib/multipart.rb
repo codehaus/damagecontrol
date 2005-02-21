c79a188c3176e6ae97c081cfa03829b3889a7503
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-02-21 01:25:40 +0000

    Some fixes to rubyforgepublisher

--- rscm/lib/multipart.rb
+++ rscm/lib/multipart.rb
@@ -2,6 +2,9 @@ require 'open-uri'
 require 'net/http'
 require 'cgi'
 
+# Adds multipart support to Net::HTTP
+# based on Code from Patrick May
+# http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/113774
 module Net
   class Param
     def initialize(k, v)
@@ -24,21 +27,21 @@ module Net
     def to_multipart
       content = File.open(@file).read
       "Content-Disposition: form-data; name=\"#{CGI::escape(@k)}\"; filename=\"#{File.basename(@file)}\"\r\n" +
-#      "Content-Transfer-Encoding: binary\r\n" +
       "Content-Type: #{@mime_type}\r\n\r\n" + content + "\r\n"
     end
   end
 
   class HTTP
 
-    def post_multipart(path, params, header={}, dest=nil, boundary="-----------------------------16558394734534412381714907510") # :yield: self  
-      data = params.collect { |p|
-        boundary + "\r\n" + p.to_multipart
-      }.join("") + boundary + "\r\n\r\n"
-      header["Content-Type"] = "multipart/form-data; boundary=" + boundary
-      header["Content-Length"] = "#{data.length}"
+    def post_multipart(path, params, header={}, dest=nil, boundary="----------ThIs_Is_tHe_bouNdaRY_$") # :yield: self  
+      body = params.collect { |p|
+        "--" + boundary + "\r\n" + p.to_multipart
+      }.join("") + "--" + boundary + "--" + "\r\n"
 
-      post(path, data, header, dest)
+      header["Content-Type"] = "multipart/form-data; boundary=" + boundary
+      header["Content-Length"] = "#{body.length}"
+
+      post(path, body, header, dest)
     end
     
     alias :old_post :post
