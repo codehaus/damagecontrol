77b520bfe71bce4a3bdfc38628a5c0c6aa7bc729
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-02-23 05:58:25 +0000

    new

---
+++ trunk/damagecontrol/test/damagecontrol/publisher/email_test.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require 'rscm/mockit'
+require 'rscm/changes'
+require 'damagecontrol/publisher/email'
+require 'damagecontrol/build'
+
+module DamageControl
+  module Publisher
+    class EmailTest < Test::Unit::TestCase
+      include MockIt
+  
+      def test_should_send_email_on_publish
+        BuildMailer.template_root = File.expand_path(File.dirname(__FILE__))
+        publisher = Email.new
+
+        project = new_mock
+        project.__expect(:name) { "mooky" }
+        build = new_mock
+        build.__expect(:project) { project }
+        
+        # not sure what to assert here...
+        assert_equal("TMail::Mail", BuildMailer.create_email(build, publisher).class.name)
+      end
+    end
+  end
+end
