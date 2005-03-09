216f14522545418990459d3946c49484678c07ea
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-03-09 02:17:42 +0000

    Fixed email publisher

--- trunk/damagecontrol/test/damagecontrol/publisher/email_test.rb
+++ trunk/damagecontrol/test/damagecontrol/publisher/email_test.rb
@@ -1,28 +1,19 @@
 require 'test/unit'
-require 'rscm/mockit'
-require 'rscm/changes'
 require 'damagecontrol/publisher/base'
+require 'damagecontrol/publisher/fixture'
 
 module DamageControl
   module Publisher
     class EmailTest < Test::Unit::TestCase
-      include MockIt
+      include Fixture
   
       def test_should_send_email_on_publish
-        project = new_mock
-        project.__setup(:name) { "mooky" }
-
-        publisher = Email.new
-                
-
-        build = new_mock
-        build.__setup(:project) { project }
-        build.__setup(:status_message) { "Kaboom" }
-        
         BuildMailer.template_root = File.expand_path(File.dirname(__FILE__) + "/../../../app/views")
-        # not sure what to assert here...
-        tmail = BuildMailer.create_email(build, publisher)
-        puts tmail.body_port.ropen.read
+        publisher = Email.new
+        tmail = BuildMailer.create_email(mock_build(true), publisher)
+        body = tmail.body_port.ropen.read
+        assert(body.index("Successful build (by aslak)"))
+        assert(body.index("path/seven"))
       end
     end
   end
--- trunk/damagecontrol/test/damagecontrol/publisher/fixture.rb
+++ trunk/damagecontrol/test/damagecontrol/publisher/fixture.rb
@@ -1,20 +1,31 @@
 require 'rscm/mockit'
+require 'rscm/changes_fixture'
 
 module DamageControl
   module Publisher
     module Fixture
+      include MockIt
+      include RSCM::ChangesFixture
+
       # Creates a mock build that can be used in other publisher tests.
       def mock_build(successful)
         project = new_mock
         project.__setup(:name) {"TestProject"}
   
-        changeset = new_mock
-        changeset.__setup(:developer) {"Aslak"}
+        setup_changes
+        changesets = RSCM::ChangeSets.new
+        changesets.add(@change1)
+        changesets.add(@change2)
+        changesets.add(@change3)
+        changesets.add(@change4)
+        changesets.add(@change5)
+        changesets.add(@change6)
+        changesets.add(@change7)
   
         build = new_mock
         build.__setup(:project) {project}
         build.__setup(:successful?) {successful}
-        build.__setup(:changeset) {changeset}
+        build.__setup(:changeset) {changesets[3]}
         build.__setup(:status_message) {successful ? "Successful" : "Failed"}
         build
       end
