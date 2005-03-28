9dc9ff60b12703dbf78db93df6e3b2f132757ae6
srbaker <srbaker@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-03-28 00:24:06 +0000

    added revision path tracing to darcs

--- rscm/lib/rscm/scm/darcs_log_parser.rb
+++ rscm/lib/rscm/scm/darcs_log_parser.rb
@@ -5,14 +5,36 @@ require 'rexml/document'
 
 module RSCM
   class DarcsLogParser
-    def parse_changeset(changeset_io)
+    def parse_changesets(io, from_identifier=Time.epoch, to_identifier=Time.infinity)
+      changesets = ChangeSets.new
+
+      doc = REXML::Document.new io
+
+      path_revisions = {}
+      doc.elements.each("//patch") { |element|
+        changeset = parse_changeset(element.to_s, path_revisions)
+        if ((from_identifier <= changeset.time) && (changeset.time <= to_identifier))
+          changesets.add(changeset)
+        end
+      }
+
+      changesets.each do |changeset|
+        changeset.each do |change|
+          current_index = path_revisions[change.path].index(change.revision)
+          change.previous_revision = path_revisions[change.path][current_index + 1]
+        end
+      end
+
+      changesets
+    end
+
+    def parse_changeset(changeset_io, path_revisions)
       changeset = ChangeSet.new
-      changeset.revision = ''
 
       doc = REXML::Document.new changeset_io
 
       doc.elements.each("patch") { |element|
-        changeset.revision = ''
+        changeset.revision = element.attributes['hash']
         changeset.developer = element.attributes['author']
         changeset.time = Time.parse(element.attributes['local_date'])
         changeset.message = element.elements["comment"].text
@@ -21,29 +43,23 @@ module RSCM
       
 
         element.elements["summary"].elements.each("add_file") { |file|
-          changeset << Change.new(file.text.strip, Change::ADDED, changeset.developer, nil, changeset.revision, changeset.time)
+          add_changes(changeset, file.text.strip, Change::ADDED, path_revisions)
         }
         element.elements["summary"].elements.each("modify_file") { |file|
-          changeset << Change.new(file.text.strip, Change::MODIFIED, changeset.developer, nil, changeset.revision, changeset.time)
+          add_changes(changeset, file.text.strip, Change::MODIFIED, path_revisions)
         }
       }
 
       changeset
     end
 
-    def parse_changesets(io, from_identifier=Time.epoch, to_identifier=Time.infinity)
-      changesets = ChangeSets.new
+  private
 
-      doc = REXML::Document.new io
+    def add_changes(changeset, path, state, path_revisions)
+      changeset << Change.new(path, state, changeset.developer, nil, changeset.revision, changeset.time)
 
-      doc.elements.each("//patch") { |element|
-        changeset = parse_changeset(element.to_s)
-        if ((from_identifier <= changeset.time) && (changeset.time <= to_identifier))
-          changesets.add(changeset)
-        end
-      }
-
-      changesets
+      path_revisions[path] ||= []
+      path_revisions[path] << changeset.revision
     end
   end
 end
