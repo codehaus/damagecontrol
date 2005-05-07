8b055378af6910aa03cc7731a959d6a48f302e37
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-05-07 00:16:58 +0000

    added more exitcode tests

--- damagecontrol/test/damagecontrol/build_test.rb
+++ damagecontrol/test/damagecontrol/build_test.rb
@@ -19,15 +19,56 @@ module DamageControl
       revision.__setup(:project) {p}
 
       t = Time.utc(1971,2,28,23,45,00)
-      build = Build.new(revision, t, "Testing")
-      
       a_program = File.expand_path(File.dirname(__FILE__) + "/a_program.rb")
-      build.execute("ruby #{a_program} 44", execute_dir, {'foo' => 'bar'})
+
+# Program exit value | $?.to_i | $? >> 8
+# 
+# -256                       0         0
+# -255                     256         1
+#   -3                   64768       253
+#   -2                   65024       254
+#   -1                   65280       255
+#    0                       0         0
+#    1                     256         1
+#    2                     512         2
+#    3                     768         3
+#  255                   65280       255
+#  256                       0         0
+# 
+
+      # EXIT -1
+      negative_build = Build.new(revision, t, "Testing")
+      negative_build.execute("ruby #{a_program} -1", execute_dir, {'foo' => 'bar'})
+
       stderr = File.expand_path("#{revision_dir}/builds/19710228234500/stderr.log")
       assert_equal("this\nis\nstderr\nbar", File.read(stderr))
       stdout = File.expand_path("#{revision_dir}/builds/19710228234500/stdout.log")
-      assert_equal("this\nis\nstdout\n44", File.read(stdout))
-      assert_equal(44, build.exit_code)
+      assert_equal("this\nis\nstdout\n-1", File.read(stdout))
+      assert_equal(255, negative_build.exit_code)
+      assert(!negative_build.successful?)
+
+      # EXIT 0
+      zero_build = Build.new(revision, t+1, "Testing")
+      zero_build.execute("ruby #{a_program} 0", execute_dir, {'foo' => 'bar'})
+
+      stderr = File.expand_path("#{revision_dir}/builds/19710228234501/stderr.log")
+      assert_equal("this\nis\nstderr\nbar", File.read(stderr))
+      stdout = File.expand_path("#{revision_dir}/builds/19710228234501/stdout.log")
+      assert_equal("this\nis\nstdout\n0", File.read(stdout))
+      assert_equal(0, zero_build.exit_code)
+      assert(zero_build.successful?)
+
+      # EXIT +1
+      positive_build = Build.new(revision, t+2, "Testing")
+      positive_build.execute("ruby #{a_program} 1", execute_dir, {'foo' => 'bar'})
+
+      stderr = File.expand_path("#{revision_dir}/builds/19710228234502/stderr.log")
+      assert_equal("this\nis\nstderr\nbar", File.read(stderr))
+      stdout = File.expand_path("#{revision_dir}/builds/19710228234502/stdout.log")
+      assert_equal("this\nis\nstdout\n1", File.read(stdout))
+      assert_equal(1, positive_build.exit_code)
+      assert(!positive_build.successful?)
+
     end
 
     def TODOtest_should_kill_long_running_build
