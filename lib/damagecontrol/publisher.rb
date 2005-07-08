c5b91d07290525fc76fc03bde2257f26b0df484a
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-07-08 05:23:38 +0000

    added more tests for trac and view_cvs

--- lib/damagecontrol/publisher.rb
+++ lib/damagecontrol/publisher.rb
@@ -1,11 +1,5 @@
 require File.dirname(__FILE__) + "/publisher/base.rb"
 
-class Class
-  def <=>(o)
-    name <=> o.name
-  end
-end
-
 Dir[File.dirname(__FILE__) + "/publisher/*.rb"].each do |src|
   require src
 end
--- lib/damagecontrol/scm_web/view_cvs.rb
+++ lib/damagecontrol/scm_web/view_cvs.rb
@@ -1,6 +1,6 @@
 module DamageControl
   module ScmWeb
-    class ViewCVS
+    class ViewCvs
       attr_accessor :baseurl
 
       def initialize(baseurl)
@@ -11,18 +11,16 @@ module DamageControl
         RSCM::PathConverter.ensure_trailing_slash(baseurl)
       end
 
-      def file_url(file, anchor=false)
-        result = nil
+      def file_url(file)
         if(file.previous_native_revision_identifier)
-          result = "#{url}#{file.path}?r1=#{file.previous_native_revision_identifier}&r2=#{file.native_revision_identifier}"
+          "#{url}#{file.path}?r1=#{file.previous_native_revision_identifier}&r2=#{file.native_revision_identifier}"
         else
           # point to the viewcvs (rev) and fisheye (r) revisions (no diff view)
-          result = "#{url}#{file.path}?rev=#{file.native_revision_identifier}&r=#{file.native_revision_identifier}"
+          "#{url}#{file.path}?rev=#{file.native_revision_identifier}&r=#{file.native_revision_identifier}"
         end
-        anchor ? "<a href=\"#{result}\">#{file.path}</a>" : result
       end
 
-      def revision_url(revision, anchor=false)
+      def revision_url(revision)
         url
       end
     end
