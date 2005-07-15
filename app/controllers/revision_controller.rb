cbb987e2fcc7bdf0f77bc1583b451cab5c0ff203
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-07-15 01:19:44 +0000

    oops

--- app/controllers/revision_controller.rb
+++ app/controllers/revision_controller.rb
@@ -1,6 +1,16 @@
 class RevisionController < ApplicationController
+  layout "application"
+
   def list
     project = Project.find(@params[:id])
     @revisions = project.revisions
   end
+  
+  def show
+    @revision = Revision.find(@params[:id])
+
+    @revisions = @revision.project.revisions
+    @template_for_left_column = "revision/list"
+  end
+
 end
