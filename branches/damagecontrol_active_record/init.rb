8cc416a9ab193ff62c245b517500aaabc6344d91
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-09-14 01:52:50 +0000

    added fork webrick options to daemon

--- branches/damagecontrol_active_record/init.rb
+++ branches/damagecontrol_active_record/init.rb
@@ -1,4 +1,3 @@
 # Bootstrap file for RubyScript2Exe
-# TODO: start daemon instead, and add a --webrick option to launch webrick forked
 # TODO: make sure the app is unzipped to same tempdir each time (see rubyscript2exe doco)
-load File.dirname(__FILE__) + '/script/server'
+load File.dirname(__FILE__) + '/script/daemon'
