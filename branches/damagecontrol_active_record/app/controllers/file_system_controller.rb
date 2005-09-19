065ad82938b4213f6197938c1ac4db17a084b3ce
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-09-19 01:27:18 +0000

    make all tests pass on win32

--- branches/damagecontrol_active_record/app/controllers/file_system_controller.rb
+++ branches/damagecontrol_active_record/app/controllers/file_system_controller.rb
@@ -1,18 +1,23 @@
-require 'pathname'
-
 class FileSystemController < ApplicationController
+  # TODO: use http://rubyforge.org/projects/mime-types/
+  MIME_TYPES = Hash.new("application/octet-stream")
+  def type
+    MIME_TYPES[extname]
+  end
+
 
   def artifacts
     @path_array = @params[:path] || []
-    file = Pathname.new(Artifact::ARTIFACT_DIR + '/' + @path_array.join('/'))
-    if(file.directory?)
-      @dir = file
+    all = [Artifact::ARTIFACT_DIR] + @path_array
+    file_name = File.join(all)
+    if(File.directory?(file_name))
+      @dir = file_name
       render :action => "dir"
     else
       # TODO: Make this work with stream. Keep getting warnings in the console:
       # /usr/local/lib/ruby/gems/1.8/gems/actionpack-1.9.1/lib/action_controller/streaming.rb:71: warning: syswrite for buffered IO
       # TODO: Look up content-type in a lookup table. maybe from the OS (if POSIX at least)
-      send_file(file.realpath, :disposition => "inline".freeze, :stream => false, :type => "text/plain".freeze)
+      send_file(file_name, :disposition => "inline".freeze, :stream => false, :type => "text/plain".freeze)
     end
   end
 
--- branches/damagecontrol_active_record/app/helpers/file_system_helper.rb
+++ branches/damagecontrol_active_record/app/helpers/file_system_helper.rb
@@ -1,2 +1,13 @@
 module FileSystemHelper
+  DEFAULT_ICON_PATH = '/images/filetypes/txt.gif'
+
+  def file_icon(file_name)
+    icon_path = nil
+    if(File.directory?(file_name))
+      icon_path = '/images/filetypes/dir.gif'
+    else
+      icon_path = '/images/filetypes/#{File.extname(file_name)}.gif'
+    end
+    File.exist?(RAILS_ROOT + '/public' + icon_path) ? icon_path : DEFAULT_ICON_PATH
+  end
 end
