34bffcd7a8f28f809bdfb958141e1092db2bd897
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-09-22 00:32:58 +0000

    removed cruise importer and improved rubyscript2exe

--- lib/damagecontrol.rb
+++ lib/damagecontrol.rb
@@ -1,3 +1,11 @@
+require_gem 'rscm'
+require_gem 'jabber4r'
+require_gem 'rake'
+require_gem 'RedCloth'
+require_gem 'ruby-growl'
+require_gem 'meta_project'
+require_gem 'mime-types'
+
 require 'rscm'
 require 'meta_project'
 require 'damagecontrol/platform'
@@ -9,12 +17,14 @@ require 'damagecontrol/meta_project/scm_web_ext'
 require 'damagecontrol/meta_project/tracker_ext'
 require 'damagecontrol/meta_project/build_tool'
 require 'damagecontrol/importer/meta_project'
-require 'damagecontrol/importer/cruise_control'
 require 'damagecontrol/scm_poller'
 require 'damagecontrol/build_daemon'
 require 'damagecontrol/core_ext/pathname'
 require 'damagecontrol/core_ext/class'
+
+require_gem 'sqlite-ruby'
 require 'damagecontrol/sqlite/retry'
+
 
 exit if defined?(REQUIRE2LIB) # rubyscript2exe pqackaging mode
 
