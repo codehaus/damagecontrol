96c0ea5735eb42446b8ea1dd7de3b18d1ba900f0
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-09-27 03:16:54 +0000

    finally figured out that gems work with rubyscript2exe. still some quirks left

--- branches/damagecontrol_active_record/lib/damagecontrol.rb
+++ branches/damagecontrol_active_record/lib/damagecontrol.rb
@@ -1,3 +1,8 @@
+# This file explicitly requires a lot of stuff.
+# This must be done here so that rubyscript2exe can
+# discover what it needs to bundle up!
+require 'damagecontrol/platform'
+
 require_gem 'rscm'
 require_gem 'jabber4r'
 require_gem 'rake'
@@ -5,10 +10,17 @@ require_gem 'RedCloth'
 require_gem 'ruby-growl'
 require_gem 'meta_project'
 require_gem 'mime-types'
+require_gem 'sqlite-ruby'
+require_gem 'x10-cm17a'
+if(DamageControl::Platform.family == "win32")
+  require 'win32/sound'
+end
+
+require 'optparse'
+require 'webrick'
 
 require 'rscm'
 require 'meta_project'
-require 'damagecontrol/platform'
 require 'damagecontrol/dom'
 require 'damagecontrol/rscm_ext/base'
 require 'damagecontrol/publisher/base'
@@ -21,10 +33,7 @@ require 'damagecontrol/scm_poller'
 require 'damagecontrol/build_daemon'
 require 'damagecontrol/core_ext/pathname'
 require 'damagecontrol/core_ext/class'
-
-require_gem 'sqlite-ruby'
 require 'damagecontrol/sqlite/retry'
 
-
-exit if defined?(REQUIRE2LIB) # rubyscript2exe pqackaging mode
+exit if defined?(REQUIRE2LIB) # rubyscript2exe packaging mode
 
