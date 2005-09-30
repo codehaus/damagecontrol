5eac305042445ca25fcef8fd66eb9f4b002e8256
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-09-30 03:32:51 +0000

    exe is working and updated to rails head

--- branches/damagecontrol_active_record/config/environment.rb
+++ branches/damagecontrol_active_record/config/environment.rb
@@ -1,17 +1,51 @@
 # Be sure to restart your webserver when you modify this file.
+
+# Uncomment below to force Rails into production mode
+# (Use only when you can't set environment variables through your web/app server)
+# ENV['RAILS_ENV'] = 'production'
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # See Rails::Configuration for options
-  
-  # SHELL_DIR is used in config/database.yml
-  SHELL_DIR = File.expand_path(Dir.pwd) unless defined? SHELL_DIR
-  if defined? DC_ENV
-    raise "DC_ENV was defined, but had no :database_yml key" unless DC_ENV[:database_yml]
-    config.database_configuration_file = DC_ENV[:database_yml] 
-  end
+  # Skip frameworks you're not going to use
+  # config.frameworks -= [ :action_web_service, :action_mailer ]
+
+  # Add additional load paths for your own custom dirs
+  # config.load_paths += %W( #{RAILS_ROOT}/app/services #{RAILS_ROOT}/app/services )
+
+  # Force all environments to use the same logger level 
+  # (by default production uses :info, the others :debug)
+  # config.log_level = :debug
+
+  # Use the database for sessions instead of the file system
+  # (create the session table with 'rake create_sessions_table')
+  # config.action_controller.session_store = :active_record_store
+
+  # Enable page/fragment caching by setting a file-based store
+  # (remember to create the caching directory and make it readable to the application)
+  # config.action_controller.fragment_cache_store = :file_store, "#{RAILS_ROOT}/cache"
+
+  # Activate observers that should always be running
+  # config.active_record.observers = :cacher, :garbage_collector
+
+  # Make Active Record use UTC-base instead of local time
+  # config.active_record.default_timezone = :utc
+
+  # See Rails::Configuration for more options
+
+  DC_DATA_DIR = RAILS_ROOT unless defined? DC_DATA_DIR
+  config.database_configuration_file = File.join(DC_DATA_DIR, 'config', 'database.yml')
 end
+
+# Add new inflection rules using the following format 
+# (all these examples are active by default):
+# Inflector.inflections do |inflect|
+#   inflect.plural /^(ox)$/i, '\1en'
+#   inflect.singular /^(ox)en/i, '\1'
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fish sheep )
+# end
 
 # Include your application configuration below
 require File.join(File.dirname(__FILE__), 'dc_environment')
