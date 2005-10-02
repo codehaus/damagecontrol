ceb3dd18cb6bce7d888cacca25dae7a4cae9ff71
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-10-02 14:49:51 +0000

    doco

--- branches/damagecontrol_active_record/app/controllers/doc_controller.rb
+++ branches/damagecontrol_active_record/app/controllers/doc_controller.rb
@@ -1,80 +1,9 @@
 class DocController < ApplicationController
+  append_before_filter :sidebar
+  
+private
 
-  def revision
-  end
-
-  def build
-  end
-
-  def build_states
-  end
-
-  def publisher
-  end
-
-  def scm_web
-  end
-
-  def tracker
-  end
-
-  def project
-  end
-
-  def installation
-  end
-
-  def database
-  end
-
-  def platform
-  end
-
-  def build_tool
-  end
-
-  def security
-  end
-
-  def scm
-  end
-
-  def trigger
-  end
-
-  def logs
-  end
-
-  def stats
-  end
-
-  def rss_atom
-  end
-
-  def faq
-  end
-
-  def api
-  end
-
-  def project_dependencies
-  end
-
-  def architecture
-  end
-
-  def web_service
-  end
-
-  def related
-  end
-
-  def source
-  end
-
-  def extending
-  end
-
-  def trouble_shooting
+  def sidebar
+    @template_for_left_column = "doc/sidebar"
   end
 end
