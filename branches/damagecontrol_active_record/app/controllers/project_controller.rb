67b67abb3a6bdddf61a1731d827c5aef43558a0f
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-10-02 16:56:52 +0000

    scripted configuration

--- branches/damagecontrol_active_record/app/controllers/project_controller.rb
+++ branches/damagecontrol_active_record/app/controllers/project_controller.rb
@@ -60,23 +60,22 @@ class ProjectController < ApplicationController
 
   def create
     project = Project.create(@params[:project])
-    populate_from_params(project)
-
-    project.update_attributes(@params[:project])
+    project.populate_from_hash(@params)
+    project.save
     redirect_to :action => "edit", :id => project.id
   end
 
   def update
     project = find
-    populate_from_params(project)
-
-    project.update_attributes(@params[:project])
+    project.populate_from_hash(@params)
+    project.save
     redirect_to :action => "edit", :id => project.id
   end
   
   def test_publisher
     project = Project.new(@params[:project])
-    populate_from_params(project)
+    project.populate_from_hash(@params)
+
     publisher_class_name = @params[:publisher_class_name]
     publisher = project.publishers.find{|p| p.class.name == publisher_class_name}
     
@@ -144,32 +143,6 @@ private
     @project ||= Project.find(@params[:id]) if @params[:id]
   end
   
-  def populate_from_params(project)
-    project.scm        = deserialize_to_array(@params[:scm]).find{|scm| scm.enabled}
-    project.tracker    = deserialize_to_array(@params[:tracker]).find{|tracker| tracker.enabled}
-    project.publishers = deserialize_to_array(@params[:publisher])
-
-    project.scm_web = MetaProject::ScmWeb::Browser.new
-    project.scm_web.dir_spec      = @params[:scm_web][:dir_spec]
-    project.scm_web.history_spec  = @params[:scm_web][:history_spec]
-    project.scm_web.raw_spec      = @params[:scm_web][:raw_spec]
-    project.scm_web.html_spec     = @params[:scm_web][:html_spec]
-    project.scm_web.diff_spec     = @params[:scm_web][:diff_spec]
-
-    project.dependencies.clear
-    if(@params[:project_dependencies])
-      @params[:project_dependencies].each do |project_id|
-        p = Project.find(project_id)
-        if(project.could_depend_on?(p))
-          project.dependencies << p
-        else
-          flash["notice"] ||= ""
-          flash["notice"] << "Can't depend on #{p.name}, it would create a cycle.<br/>"
-        end
-      end
-    end
-  end
-
   def define_plugin_rows
     # Workaround for AR bug
     @project.publishers = YAML::load(@project.publishers) if @project.publishers.class == String
