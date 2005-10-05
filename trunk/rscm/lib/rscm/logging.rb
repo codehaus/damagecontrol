f062dee502a191264819fbaf113218a332b62387
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-10-05 19:06:46 +0000

    Remove logging

--- trunk/rscm/lib/rscm/logging.rb
+++ trunk/rscm/lib/rscm/logging.rb
@@ -6,14 +6,3 @@ if(WINDOWS)
 else
   HOMEDIR = ENV['HOME']
 end
-
-begin
-  RSCM_DEFAULT_LOGGER = Logger.new("#{HOMEDIR}/.rscm.log")
-rescue StandardError
-  RSCM_DEFAULT_LOGGER = Logger.new(STDERR)
-  RSCM_DEFAULT_LOGGER.level = Logger::WARN
-  RSCM_DEFAULT_LOGGER.warn(
-    "RSCM Error: Unable to access log file. Please ensure that #{HOMEDIR}/.rscm.log exists and is chmod 0666. " +
-    "The log level has been raised to WARN and the output directed to STDERR until the problem is fixed."
-  )
-end
