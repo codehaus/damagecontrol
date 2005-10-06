220761c87d1e0720cfb7a00885d866d93877d16a
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-10-06 18:19:21 +0000

    Added first start of distributed builders

---
+++ branches/damagecontrol_active_record/java/src/main/com/buildpatterns/damagecontrol/slave/types/BuildResult.java
@@ -0,0 +1,12 @@
+package com.buildpatterns.damagecontrol.slave.types;
+
+import java.util.Date;
+
+/**
+ * @author Aslak Helles&oslash;y
+ */
+public class BuildResult {
+    public Integer exitstatus;
+    public Date begintime;
+    public Date endtime;
+}
