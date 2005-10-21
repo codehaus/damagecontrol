394f6df0ff9776ffb838305f8de38af57782013a
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-10-21 01:50:14 +0000

    added some unfinished tasks

---
+++ branches/damagecontrol_active_record/lib/damagecontrol/tasks/comment_detector.rb
@@ -0,0 +1,37 @@
+# http://ostermiller.org/findcomment.html
+
+# metric: ration of commented out code
+module DamageControl
+  module Task
+    class CommentDetector
+      C_STYLE = /((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/
+
+      def detect(dir)
+        Dir["#{dir}/**/*"].each do |f|
+          if(File.file?(f))
+            File.open(f) do |io|
+              s = io.read
+              s.scan(C_STYLE) do |c|
+                yield f, c[0]
+              end
+            end
+          end
+        end
+      end
+
+    end
+  end
+end
+
+if $0 == __FILE__
+  last_file = nil
+  DamageControl::Task::CommentDetector.new.detect(ARGV[0]) do |file, comment|
+    if(comment =~ /;/)
+      if(file != last_file)
+        puts "++++++++++++++++ #{file} ++++++++++++++++"
+        last_file = file
+      end
+      puts comment
+    end
+  end
+end
