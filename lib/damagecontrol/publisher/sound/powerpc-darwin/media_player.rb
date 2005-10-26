3eaeacef0ccd0e42f7c5cb2d485abd9e5083077e
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-10-26 03:49:23 +0000

    better sound player for os x

--- lib/damagecontrol/publisher/sound/powerpc-darwin/media_player.rb
+++ lib/damagecontrol/publisher/sound/powerpc-darwin/media_player.rb
@@ -3,14 +3,7 @@ module DamageControl
     # iTunes based player for OS X
     class MediaPlayer
       def play(track)
-        # Fork to avoid mangling env for other processes
-        pid = fork do
-          # Old OS X (pre 10.4) applescript doesn't support args on cmd line(!)
-          ENV["track_name"] = track
-          apple_script = File.dirname(__FILE__) + "/add_track.scpt"
-          IO.popen("osascript #{apple_script}")
-        end
-        Process.waitpid2(pid)
+        `playsound #{File.expand_path(track)}`
       end
     end
   end
