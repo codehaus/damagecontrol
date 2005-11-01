045d6079d8c1ee6d5b8ab7fa295c467896ccf354
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-11-01 03:15:37 +0000

    doco

--- branches/damagecontrol_active_record/app/controllers/doc_controller.rb
+++ branches/damagecontrol_active_record/app/controllers/doc_controller.rb
@@ -1,9 +1,14 @@
 class DocController < ApplicationController
   append_before_filter :sidebar
   
-private
+  def damagecontrol_yml
+    send_file(File.dirname(__FILE__) + '/../../damagecontrol.yml', :type => "text/plain", :disposition => "inline")
+  end
+ 
+ private
 
   def sidebar
     @template_for_left_column = "doc/sidebar"
   end
+  
 end
