50aa648bbad50f6907bcfe35ed8d2c04a2939a00
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-11-04 04:34:35 +0000

    rename to soundplayer

--- lib/damagecontrol/publisher/sound.rb
+++ lib/damagecontrol/publisher/sound.rb
@@ -6,19 +6,19 @@ module DamageControl
       attr_accessor :executing_sound, :successful_sound, :fixed_sound, :broken_sound, :repeatedly_broken_sound
 
       def initialize
-        @executing_sound = "hal_moment.wav"
-        @successful_sound = "hal_well.wav"
-        @fixed_sound = "better.wav"
-        @broken_sound = "hal_fault.wav"
-        @repeatedly_broken_sound = "error.wav"
+        @executing_sound         = "hal_moment.wav"
+        @successful_sound        = "hal_well.wav"
+        @fixed_sound             = "hal_better.wav"
+        @broken_sound            = "hal_fault.wav"
+        @repeatedly_broken_sound = "hal_error.wav"
       end
 
       def publish(build)
         sound = build_status_attr(build, "sound")
-        # Load platform-specific mediaplayer
-        require File.expand_path(File.dirname(__FILE__) + "/sound/" + family + "/media_player")
+        # Load platform-specific sound player
+        require File.expand_path(File.dirname(__FILE__) + "/sound/" + family + "/sound_player")
         sound_path = "#{RAILS_ROOT}/sound/#{sound}"
-        MediaPlayer.new.play(sound_path)
+        SoundPlayer.new.play(sound_path)
       end
       
     end
