37ae9d797a31af5648f9086472dc2d290b056cbd
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-11-04 04:34:35 +0000

    rename to soundplayer

--- branches/damagecontrol_active_record/lib/damagecontrol/publisher/sound/mswin32/sound_player.rb
+++ branches/damagecontrol_active_record/lib/damagecontrol/publisher/sound/mswin32/sound_player.rb
@@ -3,7 +3,7 @@ require 'win32/sound'
 module DamageControl
   module Publisher
     # win32/sound (win32utils) based player for Windows
-    class MediaPlayer
+    class SoundPlayer
       def play(track)
         Win32::Sound.play(track)
       end
--- branches/damagecontrol_active_record/lib/damagecontrol/publisher/sound/powerpc-darwin/sound_player.rb
+++ branches/damagecontrol_active_record/lib/damagecontrol/publisher/sound/powerpc-darwin/sound_player.rb
@@ -1,7 +1,7 @@
 module DamageControl
   module Publisher
-    # player for OS X
-    class MediaPlayer
+    # OS X - See bin/powerpc-darwin
+    class SoundPlayer
       def play(track)
         `playsound #{File.expand_path(track)}`
       end
