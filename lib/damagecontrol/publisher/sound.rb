4ba7a00e3d2e41aaf480adb53d57be6a87f947a8
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-11-15 04:18:05 +0000

    more sounds

--- lib/damagecontrol/publisher/sound.rb
+++ lib/damagecontrol/publisher/sound.rb
@@ -2,7 +2,7 @@ module DamageControl
   module Publisher
     class Sound < Base
 
-      attr_accessor :executing_sound, :successful_sound, :fixed_sound, :broken_sound, :repeatedly_broken_sound
+      attr_accessor :executing_sound, :successful_sound, :fixed_sound, :broken_sound, :repeatedly_broken_sound, :updating_working_copy_sound
 
       # Load platform-specific sound player
       @@sound_player = File.expand_path(File.dirname(__FILE__) + "/sound/" + DamageControl::Platform.family + "/sound_player.rb")
@@ -17,11 +17,12 @@ module DamageControl
       end
 
       def initialize
-        @executing_sound         = "hal_moment.wav"
-        @successful_sound        = "hal_well.wav"
-        @fixed_sound             = "hal_better.wav"
-        @broken_sound            = "hal_fault.wav"
-        @repeatedly_broken_sound = "hal_error.wav"
+        @executing_sound             = "hal_decision.wav"
+        @successful_sound            = "hal_well.wav"
+        @fixed_sound                 = "hal_better.wav"
+        @broken_sound                = "hal_fault.wav"
+        @repeatedly_broken_sound     = "hal_error.wav"
+        @updating_working_copy_sound = "hal_moment.wav"
       end
 
       def publish(build)
