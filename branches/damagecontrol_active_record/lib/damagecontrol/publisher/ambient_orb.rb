8a3a1cd3f8bcada85597973c49d535d0f77977a8
rinkrank <rinkrank@ce190283-b5eb-0310-b546-ff519dbb3a30>
2005-11-29 02:05:27 +0000

    improved publisher config and started gemification again

--- branches/damagecontrol_active_record/lib/damagecontrol/publisher/ambient_orb.rb
+++ branches/damagecontrol_active_record/lib/damagecontrol/publisher/ambient_orb.rb
@@ -1,13 +1,16 @@
 require 'ambient'
 
 module Ambient
+  # Opens up the Ambient::Orb class a little bit
+  # so available animations and colors can be used easily in select options
   class Orb
+    
     def self.animations
-      @@animations.to_a.collect{|kv| [kv[0], kv[1].to_s]}
+      @@animations.to_a.collect{|kv| [kv[0].to_s, kv[0].to_s]}
     end
 
     def self.colors
-      @@colors.to_a.collect{|kv| [kv[0], kv[1].to_s]}
+      @@colors.to_a.collect{|kv| [kv[0].to_s, kv[0].to_s]}
     end
   end
 end
@@ -16,14 +19,14 @@ module DamageControl
   module Publisher
     class AmbientOrb < Base
       attr_accessor :orb_id
+      attr_accessor :color
+      attr_accessor :animation
       
-      ::Build::STATES.each do |state|
-        attr_accessor state.attr_sym("", "color")
-        attr_accessor state.attr_sym("", "animation")
-      end
-
       def initialize
-        init "requesting",            "yellow", "slow"
+        @color = {}
+        @animation = {}
+
+        init "requested",             "yellow", "slow"
         init "synching_working_copy", "yellow", "fast"
         init "executing",             "yellow", "crescendo"
         init "successful",            "green",  "none"
@@ -34,16 +37,16 @@ module DamageControl
 
       def publish(build, driver=Ambient::Orb.new)
         driver.id = @orb_id 
-        driver.color = build_state_attr(build.state, "color").to_sym
-        driver.animation = build_state_attr(build.state, "animation").to_sym
+        driver.color = @color[build.state.name].to_sym
+        driver.animation = @animation[build.state.name].to_sym
         driver.update 
       end
 
     private
     
       def init(state_name, color, animation)
-        instance_variable_set("@#{state_name}_color", color)
-        instance_variable_set("@#{state_name}_animation", animation)
+        @color[state_name] = color
+        @animation[state_name] = animation
       end
 
     end
--- branches/damagecontrol_active_record/lib/damagecontrol/publisher/sound.rb
+++ branches/damagecontrol_active_record/lib/damagecontrol/publisher/sound.rb
@@ -1,35 +1,34 @@
+# Load platform-specific sound player
+@@sound_player = File.expand_path(File.dirname(__FILE__) + "/sound/" + DamageControl::Platform.family + "/sound_player.rb")
+def self.supported?
+  File.exist? @@sound_player
+end
+require @@sound_player if supported?
+
 module DamageControl
   module Publisher
     class Sound < Base
-
-      ::Build::STATES.each do |state|
-        attr_accessor state.attr_sym("", "sound")
-      end
-
-      # Load platform-specific sound player
-      @@sound_player = File.expand_path(File.dirname(__FILE__) + "/sound/" + DamageControl::Platform.family + "/sound_player.rb")
-      def self.supported?
-        File.exist? @@sound_player
-      end
-      require @@sound_player if supported?
+      attr_accessor :sound
 
       # Finds all sound files.
-      def self.files
+      def self.sounds
         Dir["#{RAILS_ROOT}/sound/*"].collect{|f| File.basename(f)}
       end
 
       def initialize
-        @requested_sound             = "hal_nothing.wav"
-        @synching_working_copy_sound = "hal_moment.wav"
-        @executing_sound             = "hal_decision.wav"
-        @successful_sound            = "hal_well.wav"
-        @fixed_sound                 = "hal_better.wav"
-        @broken_sound                = "hal_fault.wav"
-        @repeatedly_broken_sound     = "hal_error.wav"
+        @sound = {
+          'requested'             => "hal_nothing.wav",
+          'synching_working_copy' => "hal_moment.wav",
+          'executing'             => "hal_decision.wav",
+          'successful'            => "hal_well.wav",
+          'fixed'                 => "hal_better.wav",
+          'broken'                => "hal_fault.wav",
+          'repeatedly_broken'     => "hal_error.wav"
+        }
       end
 
       def publish(build)
-        sound = build_state_attr(build.state, "sound")
+        sound = @sound[build.state.name]
         # TODO: use DC_DATA_DIR (we must copy sounds out)
         sound_path = File.expand_path("#{RAILS_ROOT}/sound/#{sound}")
         raise "File not found: #{sound_path}" unless File.exist?(sound_path)
